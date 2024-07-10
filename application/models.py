926b7410438ac3e5a29f7f7725ebf4e46fa1894f
Richie Mugambi <richiemugambi4@gmail.com>
2024-07-10 20:20:16 +0300

    feat: add views for application app

--- application/models.py
+++ application/models.py
@@ -2,6 +2,7 @@
 from django.db import models
 from django.utils import timezone
 from nationalId.models import NationalId
+from license.models import License
 import uuid
 
 class DriversLicenseApplication(models.Model):
@@ -24,15 +25,20 @@ class DriversLicenseApplication(models.Model):
         ('Reissued', 'Reissued'),
     ]
 
-    application_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    application_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
     is_motor_cycle = models.BooleanField(default=False)
     is_motor_vehicle = models.BooleanField(default=False)
     certificate_number = models.IntegerField()
     applied_at = models.DateTimeField(default=timezone.now)
-    user = models.ForeignKey(NationalId, on_delete=models.CASCADE)
+    nationalId = models.ForeignKey(NationalId, on_delete=models.CASCADE)
     application_type = models.CharField(max_length=20, choices=APPLICATION_TYPE_CHOICES)
-    license_id = models.CharField(max_length=255, blank=True, null=True)
+    license = models.ForeignKey(License, on_delete=models.SET_NULL, null=True, blank=True, related_name='applications')
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Pending')
+    local_government_area = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
+    center_locations = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)  
+    phoneNumber = models.CharField(max_length=20)
 
     # Additional fields for renewal and reissue
     previous_license_id = models.CharField(max_length=255, blank=True, null=True)
@@ -40,6 +46,8 @@ class DriversLicenseApplication(models.Model):
     renewal_approved_at = models.DateTimeField(blank=True, null=True)
     reissue_applied_at = models.DateTimeField(blank=True, null=True)
     reissue_approved_at = models.DateTimeField(blank=True, null=True)
+    reissue_reason = models.TextField(blank=True, null=True)
+    reissue_police_report = models.FileField(upload_to='police_reports/', blank=True, null=True)
 
     def __str__(self):
         return str(self.application_id)
--- application/views.py
+++ application/views.py
@@ -1,53 +1,175 @@
 # views.py
-from rest_framework import generics, status
+from rest_framework import generics, status, permissions
 from rest_framework.response import Response
+from rest_framework.parsers import MultiPartParser, FormParser, JSONParser
+
 from .models import DriversLicenseApplication
+from nationalId.models import NationalId
+from license.models import License
 from .serializers import DriversLicenseApplicationSerializer
 from django.utils import timezone
+from django.shortcuts import get_object_or_404
 
 class CreateDriversLicenseApplicationView(generics.CreateAPIView):
+    permission_classes = [permissions.AllowAny]
     queryset = DriversLicenseApplication.objects.all()
     serializer_class = DriversLicenseApplicationSerializer
 
     def perform_create(self, serializer):
-        # Assign current user to the application before saving
-        serializer.save(user=self.request.user.id)  # Assuming request.user.id is the NationalId instance
+        national_id_no = self.request.data.get('nationalId')
+        try:
+            national_id = NationalId.objects.get(pk=national_id_no)
+        except NationalId.DoesNotExist:
+            raise serializers.ValidationError("NationalId does not exist")
+        
+        serializer.save(nationalId=national_id)
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
-
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-class RenewDriversLicenseApplicationView(generics.UpdateAPIView):
+class RenewDriversLicenseApplicationView(generics.CreateAPIView):
+    permission_classes = [permissions.AllowAny]  # Temporarily allow any user to access this view
     queryset = DriversLicenseApplication.objects.all()
     serializer_class = DriversLicenseApplicationSerializer
 
-    def perform_update(self, serializer):
-        # Update renewal-related fields
-        serializer.save(status='Renewal Pending', renewal_applied_at=timezone.now())
+    def create(self, request, *args, **kwargs):
+        license_id = self.kwargs.get('license_id')
+        license = get_object_or_404(License, licenseId=license_id)
 
-    def put(self, request, *args, **kwargs):
-        instance = self.get_object()
-        return self.update(request, *args, **kwargs)
+        # Get nationalId from request data
+        national_id_no = request.data.get('nationalId')
+        if not national_id_no:
+            return Response({"error": "NationalId is required."}, status=status.HTTP_400_BAD_REQUEST)
 
-class ReissueDriversLicenseApplicationView(generics.UpdateAPIView):
+        try:
+            national_id = NationalId.objects.get(pk=national_id_no)
+        except NationalId.DoesNotExist:
+            return Response({"error": "Invalid NationalId."}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Check for existing renewal application
+        existing_renewal = DriversLicenseApplication.objects.filter(
+            nationalId=national_id,
+            license=license,
+            application_type='Renewal',
+            status__in=['Renewal Pending', 'Renewal Processing']
+        ).first()
+
+        if existing_renewal:
+            return Response({"error": "A renewal application for this license is already in progress."}, status=status.HTTP_400_BAD_REQUEST)
+
+        
+        # Additional check to see if a renewal application for the specific driver's license exists
+        if DriversLicenseApplication.objects.filter(nationalId=national_id, license=license).exists():
+            return Response({"error": "A renewal application for this license is already in progress."}, status=status.HTTP_400_BAD_REQUEST)
+
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        # Create the renewal application
+        renewal_application, created = DriversLicenseApplication.objects.get_or_create(
+        nationalId=national_id,
+        license=license,
+        defaults={
+            'is_motor_cycle': request.data.get('is_motor_cycle'),
+            'is_motor_vehicle': request.data.get('is_motor_vehicle'),
+            'certificate_number': request.data.get('certificate_number'),
+            'application_type': 'Renewal',  # Hardcoded as per your requirement
+            'status': request.data.get('status'),
+            'local_government_area': request.data.get('local_government_area'),
+            'state': request.data.get('state'),
+            'center_locations': request.data.get('center_locations'),
+            'email': request.data.get('email'),
+            'phoneNumber': request.data.get('phoneNumber'),
+            'reissue_reason': request.data.get('reissue_reason'),
+            'reissue_police_report': request.data.get('reissue_police_report'),
+            'previous_license_id': request.data.get('previous_license_id'),
+            'renewal_applied_at': timezone.now(),  # Assuming current time for now
+            'renewal_approved_at': None,  
+            'reissue_applied_at': None, 
+            'reissue_approved_at': None,  
+            }
+        )
+
+        return Response({
+            "message": "Renewal application submitted successfully.",
+            "application_id": renewal_application.application_id
+        }, status=status.HTTP_201_CREATED)
+
+class ReissueDriversLicenseApplicationView(generics.CreateAPIView):
+    permission_classes = [permissions.AllowAny]
     queryset = DriversLicenseApplication.objects.all()
     serializer_class = DriversLicenseApplicationSerializer
+    parser_classes = (MultiPartParser, FormParser, JSONParser)
 
-    def perform_update(self, serializer):
-        # Update reissue-related fields
-        serializer.save(status='Reissue Pending', reissue_applied_at=timezone.now())
+    def create(self, request, *args, **kwargs):
+        license_id = request.data.get('license_id')
+        if not license_id:
+            return Response({"error": "License ID is required."}, status=status.HTTP_400_BAD_REQUEST)
 
-    def put(self, request, *args, **kwargs):
-        instance = self.get_object()
-        return self.update(request, *args, **kwargs)
+        license = get_object_or_404(License, licenseId=license_id)
 
-class ListDriversLicenseApplicationsView(generics.ListAPIView):
-    queryset = DriversLicenseApplication.objects.all()
-    serializer_class = DriversLicenseApplicationSerializer
+        # Get nationalId from request data
+        national_id_no = request.data.get('nationalId')
+        if not national_id_no:
+            return Response({"error": "NationalId is required."}, status=status.HTTP_400_BAD_REQUEST)
 
+        try:
+            national_id = NationalId.objects.get(pk=national_id_no)
+        except NationalId.DoesNotExist:
+            return Response({"error": "Invalid NationalId."}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Check for existing renewal application
+        existing_renewal = DriversLicenseApplication.objects.filter(
+            nationalId=national_id,
+            license=license,
+            application_type='Renewal',
+            status__in=['Renewal Pending', 'Renewal Processing']
+        ).exists()
+
+        if existing_renewal:
+            return Response({"error": "A renewal application for this license is already in progress."},status=status.HTTP_400_BAD_REQUEST)
+
+        # Convert boolean fields from string to boolean
+        is_motor_cycle = request.data.get('is_motor_cycle', '').lower() == 'true'
+        is_motor_vehicle = request.data.get('is_motor_vehicle', '').lower() == 'true'
+
+        # Extract reissue reason
+        reissue_reason = request.data.get('reissue_reason')
+
+        # Handle uploaded police report file
+        reissue_police_report = request.FILES.get('reissue_police_report')
+
+        # Create or update the reissue application
+        reissue_application = DriversLicenseApplication.objects.create(
+            nationalId=national_id,
+            license=license,
+            application_type='Reissue',  # Hardcoded as per your requirement
+            status='Reissue Pending',  # Assuming initial status
+            is_motor_cycle=is_motor_cycle,
+            is_motor_vehicle=is_motor_vehicle,
+            certificate_number=request.data.get('certificate_number'),
+            local_government_area=request.data.get('local_government_area'),
+            state=request.data.get('state'),
+            center_locations=request.data.get('center_locations'),
+            email=request.data.get('email'),
+            phoneNumber=request.data.get('phoneNumber'),
+            reissue_reason=reissue_reason,
+            reissue_police_report=reissue_police_report,
+            previous_license_id=request.data.get('previous_license_id'),
+            reissue_applied_at=timezone.now(),  # Assuming current time for reissue
+            reissue_approved_at=None,
+        )
+
+        return Response({
+            "message": "Reissue application submitted successfully.",
+            "application_id": reissue_application.application_id
+        }, status=status.HTTP_201_CREATED)
 
 class RetrieveDriversLicenseApplicationView(generics.RetrieveAPIView):
     queryset = DriversLicenseApplication.objects.all()
